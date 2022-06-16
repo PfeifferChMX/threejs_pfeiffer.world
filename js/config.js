440b1ad06b7cee479062d3e56581ecfa75c20803
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-06-16 17:56:16 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,26 @@
+var jetzt = new Date().getHours();
+var trabant1 = 'moon';
+var trabant2 = '';
+var trabant3 = '';
+var P1 = 'earth';
+var P2 = trabant1;
+/* 
+var sol1 = 'sun';
+var P1 = 'mercury';
+var P2 = 'venus';
+var P3 = 'earth';
+var P4 = 'mars';
+var P5 = 'jupiter';
+var P6 = 'saturn';
+var P7 = 'uranus';
+var P8 = 'neptune';
+*/
+
+if (jetzt >= 5 && jetzt <= 20) {
+bild = 'day';
+} else {
+bild = 'night';
+}
+// ueberschreiben zum testen bei bedarf:
+//bild = 'day';
+//bild = 'night';
