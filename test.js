aee467a6a8d48a1a81f2a9428a69ba720cbff1c6
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-06-16 17:57:51 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,30 +1,3 @@
-var jetzt = new Date().getHours();
-var trabant1 = 'moon';
-var trabant2 = '';
-var trabant3 = '';
-var P1 = 'earth';
-var P2 = trabant1;
-/* 
-var sol1 = 'sun';
-var P1 = 'mercury';
-var P2 = 'venus';
-var P3 = 'earth';
-var P4 = 'mars';
-var P5 = 'jupiter';
-var P6 = 'saturn';
-var P7 = 'uranus';
-var P8 = 'neptune';
-*/
-
-if (jetzt >= 5 && jetzt <= 20) {
-bild = 'day';
-} else {
-bild = 'night';
-}
-// ueberschreiben zum testen bei bedarf:
-//bild = 'day';
-//bild = 'night';
-
 
 const DEG_TO_RAD = Math.PI / 180;
 // Gen random data
