31a7bc62b2d76e1c80855d004a3b5c43c55acb8b
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2022-06-16 13:59:53 +0200

    Update test.js

--- test.js
+++ test.js
@@ -36,7 +36,7 @@ transparent: true,
 opacity: 0.1,
 alpha: true };
 
-//OrbitControl controls = new THREE.OrbitControls(camera, renderer.domElement); controls.autoRotate = true; controls.autoRotateSpeed = 2; controls.maxDistance = 350; controls.minDistance = 150; controls.enablePan = false;
+OrbitControl controls = new THREE.OrbitControls(camera, renderer.domElement); controls.autoRotate = true; controls.autoRotateSpeed = 2; controls.maxDistance = 350; controls.minDistance = 150; controls.enablePan = false;
 
     const arcsData = [...Array(N).keys()].map(() => ({
       startLat: (Math.random() - 0.5) * 180,
